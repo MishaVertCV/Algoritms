445300f9ccd93b04a5415713f96ce68e564fa52a
MishaVertCV <124257760+MishaVertCV@users.noreply.github.com>
2023-04-03 18:10:13 +0300

    Delete Яндекс видеоуроки directory

--- Яндекс видеоуроки/Counter how works.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-def stringnum(s):
-    t = Counter()
-    for symbol in s:
-        t[symbol] += 1
-    return t
-k = str(input())
-print(stringnum(k))
--- Яндекс видеоуроки/вхождение элементов строки в другую строку.py
+++
@@ -1,9 +0,0 @@
-def union(j, s):
-    k = 0
-    for stone in s:
-        if stone in j:
-            k += 1
-    return k
-a = 'df'
-f = 'dfdfdkdfgfdgkldgrokih'
-print(union(a, f))
